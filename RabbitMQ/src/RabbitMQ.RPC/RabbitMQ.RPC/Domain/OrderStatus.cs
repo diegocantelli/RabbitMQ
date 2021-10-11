9293fb627e067794dfc74eeca2e05760b61b63fc
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-11 11:05:44 -0300

    Instalando o pacote do RabbitMQ

---
+++ RabbitMQ/src/RabbitMQ.RPC/RabbitMQ.RPC/Domain/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.RPC.Domain
+{
+    public enum OrderStatus
+    {
+        Processing = 0,
+        Approved,
+        Declined
+    }
+}
