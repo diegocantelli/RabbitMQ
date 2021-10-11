5c048fa4db5f16f47d33d199848409e0f3995fdb
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-11 11:55:22 -0300

    Publicando e consumido uma mensagem no padrão produtor consumidor

--- RabbitMQ/src/RabbitMQ.Consumidor/Program.cs
+++ RabbitMQ/src/RabbitMQ.Consumidor/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMQ.Consumidor
 {
@@ -6,7 +9,47 @@ namespace RabbitMQ.Consumidor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qwebapp",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                // instanciando um consumidor que irá ler de uma determinada fila e canal
+                var consumer = new EventingBasicConsumer(channel);
+
+                // ea.Body estará o conteúdo enviado pelo produtor em bytes
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+
+                        // após execução da regra de negócio ele marca a mensagem como entregue
+                        channel.BasicAck(ea.DeliveryTag, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        // caso ocorra alguma exceção no processamento da msg, ele não confirma a msg como entregue
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+
+                //autoAck: true -> se tiver como true toda vez que ele obter um item da fila ele irá confirmar
+                // não recomendado, pois caso ocorra alguma exceção, esta mensagem será perdida
+                channel.BasicConsume(queue: "qwebapp",
+                    autoAck: true,
+                    consumer: consumer);
+            }
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
--- RabbitMQ/src/RabbitMQ.Produtor/Program.cs
+++ RabbitMQ/src/RabbitMQ.Produtor/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQ.Produtor
 {
@@ -6,7 +8,31 @@ namespace RabbitMQ.Produtor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // cria a conexao com o RabbitMQ
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                // criacao da fila
+                channel.QueueDeclare(queue: "qwebapp",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "Hello World";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "qwebapp",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine(" x Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
