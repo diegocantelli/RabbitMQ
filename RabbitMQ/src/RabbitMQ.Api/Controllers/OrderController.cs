d7c2f55c754fcdb1329ec9aa9043e8216619b703
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-12 11:16:19 -0300

    Inserindo uma msg na fila dentro de uma contrller de api rest

--- RabbitMQ/src/RabbitMQ.Api/Controllers/OrderController.cs
+++ RabbitMQ/src/RabbitMQ.Api/Controllers/OrderController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Api.Domain;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace RabbitMQ.Api.Controllers
@@ -18,10 +21,40 @@ namespace RabbitMQ.Api.Controllers
         {
             _logger = logger;
         }
+
+        [HttpPost("InsertOrder")]
         public IActionResult InsertOrder(Order order)
         {
             try
             {
+                try
+                {
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var chanel = connection.CreateModel())
+                    {
+                        chanel.QueueDeclare(queue: "orderQueue",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+                        string message = JsonSerializer.Serialize(order);
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        chanel.BasicPublish(exchange: "",
+                                            routingKey: "orderQueue",
+                                            basicProperties: null,
+                                            body: body);
+
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+                catch (Exception)
+                {
+                    _logger.LogError("Erro ao inserir na fila");
+                    throw;
+                }
                 return Accepted(order);
             }
             catch (Exception ex)
