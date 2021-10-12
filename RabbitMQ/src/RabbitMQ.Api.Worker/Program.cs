655c7e64ade380e3ac90950294bac6b238ea0dab
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-12 13:50:42 -0300

    Finalizando a implementação de comunicação com o RabbitMQ através de uma API rest e um worker lendo os itens inseridos na fila

---
+++ RabbitMQ/src/RabbitMQ.Api.Worker/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Api.Worker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ.Api.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare(queue: "orderQueue",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(chanel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var order = JsonSerializer.Deserialize<Order>(message, new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        });
+                        Console.WriteLine($" [pedido] recebido {order.ItemName}");
+
+                        chanel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception)
+                    {
+                        chanel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+
+                chanel.BasicConsume(queue: "orderQueue",
+                                    autoAck: false,
+                                    consumer: consumer);
+
+                Console.WriteLine(" Press [Enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
