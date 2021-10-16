ec005eecb7b527677b9c2b3e3e694a0010e26195
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-16 16:11:58 -0300

    Criando múltiplos consumidores, que podem compartilham o mesmo channel e conẽxão

--- RabbitMQ/src/RabbitMQ.MultiplosConsumidores/Program.cs
+++ RabbitMQ/src/RabbitMQ.MultiplosConsumidores/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
 
 namespace RabbitMQ.MultiplosConsumidores
 {
@@ -16,8 +18,28 @@ namespace RabbitMQ.MultiplosConsumidores
                     exclusive: false,
                     autoDelete: false,
                     arguments: null);
+
+                // no caso de consumidores, múltiplos consumidores podem compartilhar o mesmo channel e consequentemente
+                // a mesma connection
+                BuildWorker(channel, "Worker 1", "order");
+                BuildWorker(channel, "Worker 1", "order");
             }
             
+        }
+
+        public static void BuildWorker(IModel channel, string worker, string queue)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" {worker} Received {message}");
+            };
+
+            channel.BasicConsume(queue: queue, autoAck: true, consumer: consumer);
+
+            Console.ReadLine();
         }
     }
 }
