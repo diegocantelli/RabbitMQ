73e0983854f36919ef6110b2d0a0d4cb324ad2b3
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-24 14:32:51 -0300

    Finalizando a implementação do worker api utilizando MassTransit e RabbitMQ

--- RabbitMQ/RabbitMQ.Masstransit.WorkerApi.API/Startup.cs
+++ RabbitMQ/RabbitMQ.Masstransit.WorkerApi.API/Startup.cs
@@ -1,15 +1,10 @@
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RabbitMQ.Masstransit.WorkerApi.API.Consumer;
 
 namespace RabbitMQ.Masstransit.WorkerApi.API
 {
@@ -26,6 +21,28 @@ namespace RabbitMQ.Masstransit.WorkerApi.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddMassTransit(cfg =>
+            {
+                // informando quem será o responsável por consumir a fila
+                cfg.AddConsumer<OrderConsumer>();
+
+                cfg.UsingRabbitMq((context, config) =>
+                {
+                    config.ReceiveEndpoint("order_queue", e =>
+                    {
+                        e.ConfigureConsumer<OrderConsumer>(context);
+                    });
+
+                    config.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
